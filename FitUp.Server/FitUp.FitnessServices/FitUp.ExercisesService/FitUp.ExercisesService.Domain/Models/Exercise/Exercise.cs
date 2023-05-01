e04359dd600161f8834eff47ae9fb867012a1c0c
Georgi Vasilev <radevradoslav12@gmail.com>
2023-05-01 21:16:22 +0300

    implement initil model structure for exercises microservice

---
+++ FitUp.Server/FitUp.FitnessServices/FitUp.ExercisesService/FitUp.ExercisesService.Domain/Models/Exercise/Exercise.cs
@@ -0,0 +1,35 @@
+﻿namespace FitUp.ExercisesService.Domain.Models.Exercises
+{
+    using Models.Exercise;
+    using Models.MuscleGroup;
+    using Shared.Models;
+
+    public class Exercise : DeletableEntity<int>
+    {
+        private readonly HashSet<MuscleGroup> focusingMuscleGroups;
+
+        internal Exercise(
+            string name,
+            string description,
+            string instruction,
+            Video video,
+            DifficultyLevel difficultyLevel)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Instruction = instruction;
+            this.Video = video;
+            this.DifficultyLevel = difficultyLevel;
+
+            this.focusingMuscleGroups = new HashSet<MuscleGroup>();
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Instruction { get; private set; }
+        public Video Video { get; private set; }
+        public DifficultyLevel DifficultyLevel { get; private set; }
+        public IReadOnlyCollection<MuscleGroup> MuscleGroups
+            => focusingMuscleGroups.ToList().AsReadOnly();
+    }
+}
---
+++ FitUp.Server/FitUp.FitnessServices/FitUp.ExercisesService/FitUp.ExercisesService.Domain/Shared/Models/BaseEntity.cs
@@ -0,0 +1,58 @@
+﻿namespace FitUp.ExercisesService.Domain.Shared.Models
+{
+    public abstract class BaseEntity<TId>
+        where TId : struct
+    {
+        public TId Id { get; private set; } = default;
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is BaseEntity<TId> other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (this.GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            if (this.Id.Equals(default) || other.Id.Equals(default))
+            {
+                return false;
+            }
+
+            return this.Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(BaseEntity<TId>? first, BaseEntity<TId>? second)
+        {
+            if (first is null && second is null)
+            {
+                return true;
+            }
+
+            if (first is null || second is null)
+            {
+                return false;
+            }
+
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(BaseEntity<TId>? first, BaseEntity<TId>? second)
+        {
+            return !(first == second);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.GetType().ToString() + this.Id).GetHashCode();
+        }
+    }
+}
